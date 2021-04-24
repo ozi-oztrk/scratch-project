ce1e184f000b12d6f3af8084fcd2a95579258c66
Kerri Crawford <75552647+kerriannercrawford@users.noreply.github.com>
2021-04-24 11:07:33 -0400

    Revert "server changes"

--- webpack.config.js
+++ webpack.config.js
@@ -36,11 +36,6 @@ module.exports = {
   },
   devServer: {
     publicPath: '/public',
-    proxy: {
-      '/api': 'http://localhost:3000',
-      '/db': 'http://localhost:3000',
-      '/': 'http://localhost:3000',
-    },
     port: 8080,
     
   }
