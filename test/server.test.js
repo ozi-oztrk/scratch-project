83faee127f84d6786eac40fd3bd6b062aced5122
Allison <76595505+allisonIsCoding@users.noreply.github.com>
2021-04-28 18:54:26 -0400

    have dummy test working, writing test.

--- test/server.test.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { render } from "@testing-library/react";
-import App from "./components/app.jsx";
-
-test("renders learn react link", () => {
-  const { getByText } = render();
-  const linkElement = getByText(/welcome back to better reads/i);
-  expect(linkElement).toBeInTheDocument();
-});
