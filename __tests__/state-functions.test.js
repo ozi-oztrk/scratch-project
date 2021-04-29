6e472fc107e6bac929a366c48595932a2e854b5b
Allison <76595505+allisonIsCoding@users.noreply.github.com>
2021-04-29 10:00:04 -0400

    pushing so I can install enzyme

--- __tests__/state-functions.test.js
+++ __tests__/state-functions.test.js
@@ -1,44 +1,13 @@
-import React, { useState } from "react";
+const app = require("./../server/server.js");
+import Enzyme from "enzyme";
+import Adapter from "enzyme-adapter-react-16";
+import Book from "../client/components/Book";
 
-const STATUS = {
-  HOVERED: "hovered",
-  NORMAL: "normal",
-};
+Enzyme.configure({ adapter: new Adapter() });
 
-const Link = ({ page, children }) => {
-  const [status, setStatus] = useState(STATUS.NORMAL);
+console.log(Book);
 
-  const onMouseEnter = () => {
-    setStatus(STATUS.HOVERED);
-  };
-
-  const onMouseLeave = () => {
-    setStatus(STATUS.NORMAL);
-  };
-
-  return (
-    <a
-      className={status}
-      href={page || "#"}
-      onMouseEnter={onMouseEnter}
-      onMouseLeave={onMouseLeave}
-    >
-      {children}
-    </a>
-  );
-};
-
-export default Link;
-
-// import React from "react";
-// import { render } from "@testing-library/react";
-// import App from "../components/app.jsx";
-
-// test("renders welcome back to better reads", () => {
-//   const { getByText } = render();
-//   const element = getByText(/welcome back to better reads/i);
-//   expect(element).toBeInTheDocument();
-// });
+// describe("", () => {});
 
 // describe("Addition", () => {
 //   it("knows that 2 and 2 make 4", () => {
