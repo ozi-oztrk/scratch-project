cb1ada06547567c7daba0c7cea637e41ea5adb4f
Allison <76595505+allisonIsCoding@users.noreply.github.com>
2021-04-29 12:36:12 -0400

    working test in test file

--- __tests__/state-functions.test.js
+++ __tests__/state-functions.test.js
@@ -1,11 +1,47 @@
-const app = require("./../server/server.js");
+// const app = require("./../server/server.js");
+import React from "react";
 import Enzyme from "enzyme";
 import Adapter from "enzyme-adapter-react-16";
 import Book from "../client/components/Book";
+import SignupForm from "../client/components/SignupForm";
+import Main from "../client/components/Main";
 
 Enzyme.configure({ adapter: new Adapter() });
 
-console.log(Book);
+describe("<Main />", () => {
+  it("should have a header called 'Welcome back to Better Reads'", () => {
+    const wrapper = Enzyme.shallow(<Main />);
+    const actual = wrapper.find("h2").text();
+    const expected = "Welcome back to Better Reads!";
+
+    expect(actual).toEqual(expected);
+  });
+});
+
+xdescribe("<Book />", () => {
+  let wrapper;
+  const setState = jest.fn();
+  const useStateSpy = jest.spyOn(React, "useState");
+  useStateSpy.mockImplementation((init) => [init, setState]);
+
+  beforeEach(() => {
+    wrapper = Enzyme.shallow(<Book />);
+  });
+
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
+  xdescribe("book complete button changes state to true", () => {
+    it("calls setComplete with true", () => {
+      // wrapper.find(`#complete-button`).props().onClick();
+      wrapper.find(`#complete-button`).simulate("click", {
+        target: { value: "true" },
+      });
+      // expect(setState).toHaveBeenCalledWith(true);
+    });
+  });
+});
 
 // describe("", () => {});
 
