0b754bb08edf9cd3539219a4830111dc37bbf55e
Allison <76595505+allisonIsCoding@users.noreply.github.com>
2021-04-29 08:55:31 -0400

    testing file changes

--- __tests__/state-functions.test.js
+++ __tests__/state-functions.test.js
@@ -1,3 +1,35 @@
+import React, { useState } from "react";
+
+const STATUS = {
+  HOVERED: "hovered",
+  NORMAL: "normal",
+};
+
+const Link = ({ page, children }) => {
+  const [status, setStatus] = useState(STATUS.NORMAL);
+
+  const onMouseEnter = () => {
+    setStatus(STATUS.HOVERED);
+  };
+
+  const onMouseLeave = () => {
+    setStatus(STATUS.NORMAL);
+  };
+
+  return (
+    <a
+      className={status}
+      href={page || "#"}
+      onMouseEnter={onMouseEnter}
+      onMouseLeave={onMouseLeave}
+    >
+      {children}
+    </a>
+  );
+};
+
+export default Link;
+
 // import React from "react";
 // import { render } from "@testing-library/react";
 // import App from "../components/app.jsx";
@@ -8,8 +40,8 @@
 //   expect(element).toBeInTheDocument();
 // });
 
-describe("Addition", () => {
-  it("knows that 2 and 2 make 4", () => {
-    expect(2 + 2).toBe(4);
-  });
-});
+// describe("Addition", () => {
+//   it("knows that 2 and 2 make 4", () => {
+//     expect(2 + 2).toBe(4);
+//   });
+// });
